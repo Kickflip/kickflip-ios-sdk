d52b835a0108b6b70851752260f0341400fe044c
Chris Ballinger <chrisballinger@gmail.com>
2014-04-07 13:26:56 -0700

    More stream cell UI

--- Kickflip/Views/KFStreamTableViewCell.h
+++ Kickflip/Views/KFStreamTableViewCell.h
@@ -9,15 +9,20 @@
 #import <UIKit/UIKit.h>
 @class KFStream;
 
+typedef void(^KFStreamTableViewCellActionBlock)(void);
+
 @interface KFStreamTableViewCell : UITableViewCell
 
 @property (nonatomic, strong) UIImageView *thumbnailImageView;
 @property (nonatomic, strong) UILabel *locationLabel;
 @property (nonatomic, strong) UILabel *dateLabel;
 @property (nonatomic, strong) UILabel *durationLabel;
+@property (nonatomic, strong) UIButton *actionButton;
+@property (nonatomic, copy) KFStreamTableViewCellActionBlock actionBlock;
 
 - (void) setStream:(KFStream*)stream;
 
 + (NSString*) cellIdentifier;
++ (CGFloat) defaultHeight;
 
 @end
--- Kickflip/Views/KFStreamTableViewCell.m
+++ Kickflip/Views/KFStreamTableViewCell.m
@@ -10,6 +10,10 @@
 #import "UIImageView+AFNetworking.h"
 #import "KFStream.h"
 #import "UIView+AutoLayout.h"
+#import "KFDateUtils.h"
+
+static const NSUInteger kKFStreamTableViewCellLabelHeight = 20.0f;
+static const NSUInteger kKFStreamTableViewCellPadding = 5.0f;
 
 static NSString * const KFStreamTableViewCellIdentifier = @"KFStreamTableViewCellIdentifier";
 
@@ -19,65 +23,94 @@ static NSString * const KFStreamTableViewCellIdentifier = @"KFStreamTableViewCel
 {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
+        [self setupThumbnailView];
         [self setupDateLabel];
         [self setupLocationLabel];
-        [self setupThumbnailView];
         [self setupDurationLabel];
+        [self setupActionButton];
     }
     return self;
 }
                           
 - (void) setupDateLabel {
     self.dateLabel = [[UILabel alloc] init];
+    self.dateLabel.translatesAutoresizingMaskIntoConstraints = NO;
     [self.contentView addSubview:self.dateLabel];
+    
+    [self.dateLabel autoSetDimension:ALDimensionHeight toSize:kKFStreamTableViewCellLabelHeight];
+    [self.dateLabel autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:self.thumbnailImageView withOffset:kKFStreamTableViewCellPadding];
+    [self.dateLabel autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:kKFStreamTableViewCellPadding];
+}
+
+- (void) setupActionButton {
+    self.actionButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    self.actionButton.translatesAutoresizingMaskIntoConstraints = NO;
+    UIImage *buttonImage = [UIImage imageNamed:@"KFStreamTableViewCellDots"];
+    [self.actionButton setImage:buttonImage forState:UIControlStateNormal];
+    [self.actionButton addTarget:self action:@selector(actionButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    [self.contentView addSubview:self.actionButton];
+    
+    [self.actionButton autoSetDimensionsToSize:CGSizeMake(buttonImage.size.width + 20, buttonImage.size.height + 10)];
+    [self.actionButton autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:self.durationLabel withOffset:0];
+    [self.actionButton autoPinEdgeToSuperviewEdge:ALEdgeRight withInset:0];
 }
 
 - (void) setupDurationLabel {
     self.durationLabel = [[UILabel alloc] init];
+    self.durationLabel.translatesAutoresizingMaskIntoConstraints = NO;
+
     [self.contentView addSubview:self.durationLabel];
+    
+    [self.durationLabel autoSetDimension:ALDimensionHeight toSize:kKFStreamTableViewCellLabelHeight];
+    [self.durationLabel autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:self.thumbnailImageView withOffset:kKFStreamTableViewCellPadding];
+    [self.durationLabel autoPinEdgeToSuperviewEdge:ALEdgeRight withInset:kKFStreamTableViewCellPadding];
+}
+
+- (void) actionButtonPressed:(id)sender {
+    if (self.actionBlock) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.actionBlock();
+        });
+    }
 }
 
 - (void) setupLocationLabel {
     self.locationLabel = [[UILabel alloc] init];
+    self.locationLabel.translatesAutoresizingMaskIntoConstraints = NO;
     [self.contentView addSubview:self.locationLabel];
+    
+    [self.durationLabel autoSetDimension:ALDimensionHeight toSize:kKFStreamTableViewCellLabelHeight];
+    [self.locationLabel autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:self.dateLabel withOffset:kKFStreamTableViewCellPadding];
+    [self.locationLabel autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:kKFStreamTableViewCellPadding];
 }
 
 - (void) setupThumbnailView {
     self.thumbnailImageView = [[UIImageView alloc] init];
     self.thumbnailImageView.translatesAutoresizingMaskIntoConstraints = NO;
     [self.contentView addSubview:self.thumbnailImageView];
-    NSArray *constraints = [self.thumbnailImageView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsMake(5, 5, 5, 5) excludingEdge:ALEdgeBottom];
-    [self.contentView addConstraints:constraints];
-    NSLayoutConstraint *constraint = [self.thumbnailImageView autoSetDimension:ALDimensionHeight toSize:180];
-    [self.contentView addConstraint:constraint];
-}
-
-- (void)awakeFromNib
-{
-    // Initialization code
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated
-{
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
+    [self.thumbnailImageView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsMake(kKFStreamTableViewCellPadding, kKFStreamTableViewCellPadding, kKFStreamTableViewCellPadding, kKFStreamTableViewCellPadding) excludingEdge:ALEdgeBottom];
+    [self.thumbnailImageView autoSetDimension:ALDimensionHeight toSize:180];
 }
 
 - (void) setStream:(KFStream *)stream {
-    self.dateLabel.text = @"4/19/1900 4:19pm";
-    self.locationLabel.text = @"Berkeley, CA";
-    self.durationLabel.text = @"4m 19s";
+    self.dateLabel.text = [[KFDateUtils localizedDateFormatter] stringFromDate:stream.startDate];
+    self.locationLabel.text = stream.city;
+    self.durationLabel.text = @"4m 20s";
     [self.thumbnailImageView setImageWithURL:stream.thumbnailURL];
 }
 
 - (void) prepareForReuse {
     [super prepareForReuse];
     self.thumbnailImageView.image = nil;
+    self.actionBlock = nil;
 }
 
 + (NSString*) cellIdentifier {
     return KFStreamTableViewCellIdentifier;
+}
+
++ (CGFloat) defaultHeight {
+    return 240.;
 }
 
 @end
